b5b05b41b2f64b69a82f002776e524cbd6cf787f
stfsy <s.pfaffel@gmail.com>
2020-04-13 20:12:15 +0200

    refactor: move img paths to fixtures.js

---
+++ test/fixtures.js
@@ -0,0 +1,10 @@
+'use strict'
+
+module.exports = {
+    path: 'test/fixtures/app',
+    buildPath: 'test/fixtures/dist',
+    jpgs: ['test.jpg', 'test-uppercase.JPG'],
+    gifs: ['test.gif', 'test-uppercase.GIF'],
+    pngs: ['test.png', 'nested/nested/nested.png', 'test-uppercase.PNG'],
+    svgs: ['test.svg']
+}
--- test/specs/minifier.spec.js
+++ test/specs/minifier.spec.js
@@ -4,28 +4,11 @@ const Minifier = require('../../lib/minifier')
 const expect = require('chai').expect
 const resolve = require('path').resolve
 const fs = require('fs')
+const fixtures = require('../fixtures.js')
 
 describe('Minifier', () => {
 
     let minifier = null
-    let jpgs = []
-    let pngs = []
-    let gifs = []
-    let svgs = []
-
-    before(() => {
-        jpgs.push(('test/fixtures/app/test.jpg'))
-        jpgs.push(('test/fixtures/app/test-uppercase.JPG'))
-
-        gifs.push(('test/fixtures/app/test.gif'))
-        gifs.push(('test/fixtures/app/test-uppercase.GIF'))
-
-        pngs.push(('test/fixtures/app/test.png'))
-        pngs.push(('test/fixtures/app/nested/nested/nested.png'))
-        pngs.push(('test/fixtures/app/test-uppercase.PNG'))
-
-        svgs.push(('test/fixtures/app/test.svg'))
-    })
 
     beforeEach(() => {
         minifier = new Minifier({
@@ -35,7 +18,7 @@ describe('Minifier', () => {
 
     describe('.matches', () => {
         it('matches jpg images', () => {
-            jpgs.forEach(jpg => expect(minifier.matches(jpg)).to.be.true)
+            fixtures.jpgs.forEach(jpg => expect(minifier.matches(jpg)).to.be.true)
         })
 
         it('matches jpg images in root folders', () => {
@@ -43,15 +26,15 @@ describe('Minifier', () => {
         })
 
         it('matches gif images', () => {
-            gifs.forEach(gif => expect(minifier.matches(gif)).to.be.true)
+            fixtures.gifs.forEach(gif => expect(minifier.matches(gif)).to.be.true)
         })
 
         it('matches png images', () => {
-            pngs.forEach(png => expect(minifier.matches(png)).to.be.true)
+            fixtures.pngs.forEach(png => expect(minifier.matches(png)).to.be.true)
         })
 
         it('matches svg images', () => {
-            svgs.forEach(svg => expect(minifier.matches(svg)).to.be.true)
+            fixtures.svgs.forEach(svg => expect(minifier.matches(svg)).to.be.true)
         })
 
         it('does not match docx files', () => {
@@ -61,31 +44,31 @@ describe('Minifier', () => {
 
     describe('.minify', () => {
         const minifyAndCompareBufferSize = (file, expectedRatio) => {
-            const inBuffer = fs.readFileSync(file)
+            const inBuffer = fs.readFileSync(fixtures.path + '/' + file)
             return minifier.minifyBuffer(inBuffer).then((outBuffer) => {
                 expect(outBuffer.length / inBuffer.length).to.be.below(expectedRatio)
             })
         }
         it('should minify jpeg images', () => {
-            return Promise.all(jpgs.map((jpg) => {
+            return Promise.all(fixtures.jpgs.map((jpg) => {
                 return minifyAndCompareBufferSize(jpg, 0.5)
             }))
         })
 
         it('should minify gif images', () => {
-            return Promise.all(gifs.map((gif) => {
+            return Promise.all(fixtures.gifs.map((gif) => {
                 return minifyAndCompareBufferSize(gif, 0.75)
             }))
         })
 
         it('should minify png images', () => {
-            return Promise.all(pngs.map((png) => {
+            return Promise.all(fixtures.pngs.map((png) => {
                 return minifyAndCompareBufferSize(png, 0.5)
             }))
         })
 
         it('should minify svg images', () => {
-            return Promise.all(svgs.map((svg) => {
+            return Promise.all(fixtures.svgs.map((svg) => {
                 return minifyAndCompareBufferSize(svg, 0.55)
             }))
         })
